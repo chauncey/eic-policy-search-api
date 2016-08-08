c15a30b8a3f70c86fe8d7c373df92648e44e023b
chauncey <cchauncey@gmail.com>
2016-08-07 20:41:16 -0500

    Add tests

---
+++ test_app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import app as eic_status
+import unittest
+
+class EicPolicySearch(unittest.TestCase):
+
+    def setUp(self):
+        self.app = eic_status.app.test_client()
+        self.version = "v1.0"
+
+    def test_echo(self):
+        '''Test that echo returns a result'''
+        resp = self.app.get("/echo")
+        assert 'result' in resp.data.decode('utf-8')
+
+    def test_hello_world(self):
+        '''Test the obligatory Hello, World response'''
+        resp = self.app.get("/")
+        assert 'Hello, World!' == resp.data.decode('utf-8')
+
+
+if __name__ == "__main__":
+    unittest.main()
